97b937f311516d6930e05638c3bed6d890557e18
ed12al <ed12al@hotmail.com>
2016-04-21 17:56:14 -0700

    a

--- src/main/java/edu/csula/datascience/acquisition/MovieCollectorApp.java
+++ src/main/java/edu/csula/datascience/acquisition/MovieCollectorApp.java
@@ -1,7 +1,11 @@
 package edu.csula.datascience.acquisition;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 import org.jsoup.nodes.Element;
 
@@ -10,10 +14,25 @@ public class MovieCollectorApp {
 	public static void main(String[] args) throws ParseException {
 		
 		MovieCollector collector = new MovieCollector();
-		MovieSource source = new MovieSource("2016-04-15");
-		Collection<Element> elements = source.getSource();
-		if(elements != null){
-			Collection<Movie> movies = collector.mungee(elements);
+		List<String> dates = getDates("2016-03-15", "2016-04-05");
+		for(String date: dates){
+			MovieSource source = new MovieSource(date);
+			Collection<Element> elements = source.getSource();
+			if(elements != null){
+				Collection<Movie> movies = collector.mungee(elements);
+			}
 		}
 	}
+	
+	private static List<String> getDates(String datefrom, String dateto) throws ParseException{
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date dateFrom = format.parse(datefrom);
+		Date dateTo = format.parse(dateto);
+		List<String> dates = new ArrayList<String>();
+		for(long date = dateFrom.getTime(); date < dateTo.getTime(); date+=86400000){
+			dates.add(format.format(new Date(date)));
+		}
+		return dates;
+	}
+	
 }
